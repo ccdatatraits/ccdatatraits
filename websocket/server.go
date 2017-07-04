e6f50919ca4f352af954fbc79d236158b5af65e7
Sohail M Khan <sokhan@mediamath.com>
2017-07-04 11:08:52 +1000

    Jacob Martin's gorilla/websocket implementation Successfully modifying server.go to use gorilla/websocket html/index.html at default location
    Signed-off-by: Sohail M Khan <sokhan@mediamath.com>

--- websocket/server.go
+++ websocket/server.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"strings"
+	"net/http"
+	"os"
+	"io/ioutil"
+	"log"
+	"time"
+	"github.com/gorilla/websocket"
 )
 
 const (
-	DEFAULT_LISTENADDR = ":3000"
-	DEFAULT_ROOTHTMLLOCATION = "html/index.html"
+	DEFAULT_LISTEN_ADDR        = ":3000"
+	DEFAULT_ROOT_HTML_LOCATION = "html/index.html"
 )
 
 type ServerConfig struct {
-	ListenAddr string
+	ListenAddr       string
 	RootHTMLLocation string
 }
 type ServerService interface {
@@ -19,55 +25,101 @@ type ServerService interface {
 	NewServer() Server
 }
 type Server interface {
-	Start() Server
+	Start() error
 	GetCommChannel() (chan string)
-	HasStopped() bool
-	Stop() string
+	Stop() error
 }
 
-type ServerServiceImpl struct {
+type serverServiceImpl struct {
 	savedConfiguration ServerConfig
 }
-func (service *ServerServiceImpl) Configure(config ServerConfig) ServerService {
+
+func (service *serverServiceImpl) Configure(config ServerConfig) ServerService {
 	savedServerListenAddr := strings.Trim(config.ListenAddr, " ")
 	if "" == savedServerListenAddr {
-		service.savedConfiguration.ListenAddr = DEFAULT_LISTENADDR
+		service.savedConfiguration.ListenAddr = DEFAULT_LISTEN_ADDR
 	} else {
 		service.savedConfiguration.ListenAddr = savedServerListenAddr
 	}
-	savedServerRootHTMLLocation := strings.Trim(config.ListenAddr, " ")
+	savedServerRootHTMLLocation := strings.Trim(config.RootHTMLLocation, " ")
 	if "" == savedServerRootHTMLLocation {
-		service.savedConfiguration.RootHTMLLocation = DEFAULT_ROOTHTMLLOCATION
+		service.savedConfiguration.RootHTMLLocation = DEFAULT_ROOT_HTML_LOCATION
 	} else {
 		service.savedConfiguration.RootHTMLLocation = savedServerRootHTMLLocation
 	}
 	return service
 }
-func (service *ServerServiceImpl) NewServer() Server {
-	newServerImpl := new(ServerImpl)
+func (service *serverServiceImpl) NewServer() Server {
+	newServerImpl := new(serverImpl)
 	newServerImpl.configuration = service.savedConfiguration
 	return newServerImpl
 }
 
-type ServerImpl struct {
+type httpResWriterReqPointer struct {
+	w http.ResponseWriter
+	r *http.Request
+}
+
+type serverImpl struct {
 	configuration ServerConfig
 	serverChannel chan string
 	serverStopped bool
-	stopChannel chan struct{}
+	stopChannel   chan struct{}
+	conn          httpResWriterReqPointer
+	done          chan bool
 }
-func (server *ServerImpl) Start() Server {
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+}
+
+func (server *serverImpl) Start() error {
 	fmt.Println("Configured for address:", server.configuration.ListenAddr)
 	fmt.Println("Root HTML location:", server.configuration.RootHTMLLocation)
-	fmt.Println("Running server now\n")
+	fmt.Println("Running server now")
+	fmt.Println("")
+	http.HandleFunc("/", server.rootHandler)
+	http.HandleFunc("/websocket", server.socketHandler)
+	err := http.ListenAndServe(server.configuration.ListenAddr, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
+func (server *serverImpl) rootHandler(w http.ResponseWriter, r *http.Request) {
+	indexFile, err := os.Open(server.configuration.RootHTMLLocation)
+	if err != nil {
+		fmt.Println(err)
+	}
+	index, err := ioutil.ReadAll(indexFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Fprintf(w, string(index))
+}
+func (server *serverImpl) socketHandler(w http.ResponseWriter, r *http.Request) {
 	server.serverChannel = make(chan string)
 	server.stopChannel = make(chan struct{})
+	server.conn = httpResWriterReqPointer{w, r}
+	server.done = make(chan bool)
 	go server.serverChannelHandler()
-	return server
+	<-server.done
 }
-func (server *ServerImpl) GetCommChannel() (chan string) {
+func (server *serverImpl) GetCommChannel() (chan string) {
 	return server.serverChannel
 }
-func (server *ServerImpl) serverChannelHandler() {
+func (server *serverImpl) serverChannelHandler() {
+	const (
+		TOTAL = 10
+	)
+	var OUT_MSGS = [2]string{"ping", "pong"}
+	i := 0
+	conn, err := upgrader.Upgrade(server.conn.w, server.conn.r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 HERE:
 	for {
 		select {
@@ -83,42 +135,56 @@ HERE:
 				server.serverChannel <- "PONG!"
 			case "ALIVE?":
 				server.serverChannel <- "YES!"
-			case "STOP!":
-				go server.Stop()
 			default:
 				server.serverChannel <- incoming
 			}
+		default:
+			msgType, msg, err := conn.ReadMessage()
+			incoming_msg := string(msg)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if incoming_msg == "ping" || incoming_msg == "pong" {
+				fmt.Println(incoming_msg)
+				time.Sleep(500 * time.Millisecond)
+				err = conn.WriteMessage(msgType, []byte(OUT_MSGS[i%2]))
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			} else {
+				conn.Close()
+				fmt.Println(incoming_msg)
+				return
+			}
 		}
+		time.Sleep(500 * time.Millisecond)
+		i++
 	}
 	fmt.Println("Server Comm Channel stopped!")
-	server.stopChannel <- struct{}{}
-	close(server.stopChannel)
+	//server.stopChannel <- struct{}{}
+	//close(server.stopChannel)
+	conn.Close()
+	<-server.done
+	close(server.done)
 }
-func (server *ServerImpl) HasStopped() bool {
-	return server.serverStopped
-}
-func (server *ServerImpl) Stop() string {
-	if server.HasStopped() {
-		return "Server has already been stopped!"
-	}
+func (server *serverImpl) Stop() error {
 	server.stopChannel <- struct{}{}
-	return "Sent back:STOPPED!"
+	<-server.stopChannel
+	return nil
 }
 func handleMessage(server Server, msg string) string {
-	if server.HasStopped() {
-		return "Server has already been stopped!"
-	}
 	server.GetCommChannel() <- msg
 	return "Sent back:" + <-server.GetCommChannel()
 }
 
 func main() {
-	server := new(ServerServiceImpl).Configure(ServerConfig{}).NewServer()
-	serverCommChan := server.Start()
-	fmt.Println(handleMessage(serverCommChan, "PING!"))
-	fmt.Println(handleMessage(serverCommChan, "ALIVE?"))
-	fmt.Println(handleMessage(serverCommChan, "STOP!"))
-	fmt.Println(server.Stop())
+	server := new(serverServiceImpl).Configure(ServerConfig{}).NewServer()
+	server.Start()
+	//fmt.Println(handleMessage(server, "PING!"))
+	//fmt.Println(handleMessage(server, "ALIVE?"))
+	//server.Stop()
 
 	fmt.Println("\nENDED!")
 }
